8a64607ba5430d5752e85a2ee68954c4368ccc8b
alfonseca28 <alfonzka_13@hotmail.com>
2023-05-23 23:54:33 -0600

    Funcionan el registro de los usuarios y sus validaciones tanto del correo, la contraseña y el nombre de usuario, tanto desde el frontend como desde el backend con php

--- pay_checkout.php
+++ pay_checkout.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -8,16 +9,17 @@
     <!-- Con este script mandamos a llamar las credenciales del API de Paypal, para esto se pone el client ID y con esto tenemos la funcionalidad de realizar pagos con Paypal -->
     <script src="https://www.paypal.com/sdk/js?client-id=AZNkKB0lE0eee5PHguiAyqhdDvDNCAQDjDms638xSwxMVpzfZPwwnu9SKSgOhisUDIswRwkyTUfuV4dI&currency=MXN"></script>
 </head>
+
 <body>
     <!-- Aqui se crea el boton del metodo de pago con Paypal, ademas le agrego un estilo para que tenga esquinas redondeadas -->
     <div id="paypal-button-container"></div>
     <script>
         paypal.Buttons({
-            style:{
+            style: {
                 shape: 'pill',
                 label: 'pay'
             },
-            createOrder: function(data, actions){
+            createOrder: function(data, actions) {
                 return actions.order.create({
                     purchase_units: [{
                         amount: {
@@ -27,19 +29,20 @@
                 });
             },
             // A partir de aqui comienza lo que es la transacion al momento de pagar y que por consola del navegador nos muestre todos los detalles de la transacion
-            onApprove: function(data, actions){
-                actions.order.capture().then(function(detalles){
+            onApprove: function(data, actions) {
+                actions.order.capture().then(function(detalles) {
                     console.log(detalles);
                     // Finalmente aqui se redirecciona a otra ventana para mostrar que se realizo correctamente el pago
-                    window.location.href="pay_completed.html"
+                    window.location.href = "pay_completed.html"
                 });
             },
             // Esta funcion es para que al momento de cerrar la ventana emergente del pago, se cancele y nos muestre una alerta de que se cancelo el pago
-            onCancel: function(data){
+            onCancel: function(data) {
                 alert("Pago cancelado");
                 console.log(data);
             }
         }).render('#paypal-button-container');
     </script>
 </body>
+
 </html>
