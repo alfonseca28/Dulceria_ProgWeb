92be4c1ab187e6416b35d42cdc99790e7733304b
alfonseca28 <alfonzka_13@hotmail.com>
2023-06-07 20:33:36 -0600

    Proyecto terminado, pagina web lista

--- admin.php
+++ admin.php
@@ -11,6 +11,8 @@ require_once("config/database.php");
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Dulcería - Purrfections</title>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css" integrity="sha512-cZkXAVCNOm1wlnw8yMO3acNFbcZBD1vMQAOc8iFsHvvYBf7TN9z2Ji2U7H77csSRVhM8Ks3b8dMO/ZLxQ7GFFg==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+
     <!-- Se crea el link para que la pagina pueda realizar las utilidades de bootstrap y poder tomar algunos ejemplos de css -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
     <!-- Se incluye la hoja de estilos a la pagina -->
@@ -38,21 +40,22 @@ require_once("config/database.php");
             </div>
         </div>
     </header>
-
     <main>
         <div class="container mt-4">
-            <div class="row">
-                <div class="col-12 col-md-4">
-                    <a href="clases\reporte_compras.php" class="btn btn-primary btn-lg btn-block mb-3">Ver compras</a>
+            <div class="row justify-content-center">
+                <div class="col-12 col-md-4 d-flex justify-content-center">
+                    <a href="clases\reporte_compras.php" class="btn btn-primary btn-lg">Ver compras</a>
                 </div>
-                <div class="col-12 col-md-4">
-                    <a href="clases\reporte_productos.php" class="btn btn-primary btn-lg btn-block mb-3">Ver productos</a>
+                <div class="col-12 col-md-4 d-flex justify-content-center">
+                    <a href="clases\reporte_productos.php" class="btn btn-primary btn-lg">Ver productos</a>
                 </div>
-                <div class="col-12 col-md-4">
-                    <a href="clases\add_productos.php" class="btn btn-primary btn-lg btn-block mb-3">Agregar productos</a>
+                <div class="col-12 col-md-4 d-flex justify-content-center">
+                    <a href="clases\add_productos.php" class="btn btn-primary btn-lg">Agregar productos</a>
                 </div>
             </div>
         </div>
+
+
     </main>
 
     <!-- Con el siguiente script se nos dara la facilidad de darle funcionalidad de javascript -->
